be4d3da6473c1df8b1a85832ca1c0ec30236f814
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-01-26 20:12:51 -0300

    Made a connection with a MySQL DB through a Docker image.

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -2,35 +2,38 @@ import { Controller, Get, Post, Delete, Put, Param, Req, Body } from '@nestjs/co
 import { BooksService } from './books.service';
 import { Request } from 'express';
 import { BookDto } from './book.dto';
-import { Book } from './book.class';
+import { Book } from './book.entity';
 
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) {}
 
   @Get()
-  findAll(@Req() request: Request): Book[] {
+  findAll(@Req() request: Request): Promise<Book[]> {
     console.log(request.query);
     return this.booksService.findAll(request.query);
   }
 
   @Get(':bookId')
-  findBook(@Param('bookId') bookId: string): Book {
+  findBook(@Param('bookId') bookId: string): Promise<Book> {
     return this.booksService.findBook(bookId);
   }
 
   @Post()
-  createBook(@Body() newBook: BookDto): Book {
+  createBook(@Body() newBook: BookDto): Promise<Book> {
     return this.booksService.createBook(newBook);
   }
 
   @Delete(':bookId')
-  deleteBook(@Param('bookId') bookId: string): Book {
+  deleteBook(@Param('bookId') bookId: string): Promise<Book> {
     return this.booksService.deleteBook(bookId);
   }
 
   @Put(':bookId')
-  updateBook(@Param('bookId') bookId: string, @Body() newBook: BookDto): Book {
+  updateBook(
+    @Param('bookId') bookId: string,
+    @Body() newBook: BookDto,
+  ): Promise<Book> {
     return this.booksService.updateBook(bookId, newBook);
   }
 }
